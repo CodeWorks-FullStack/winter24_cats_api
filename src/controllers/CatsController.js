b35630f45145e7c763a3597f1c5753552e8ff2de
Jeremy Fowler <jembyfowler@gmail.com>
2024-11-18 12:30:23 -0700

    added try catches

--- src/controllers/CatsController.js
+++ src/controllers/CatsController.js
@@ -4,12 +4,25 @@ import BaseController from "../utils/BaseController.js";
 export class CatsController extends BaseController {
   constructor() {
     super('api/cats')
-    this.router.get('', this.getAllCats)
+    this.router.get('', this.getAllCats).post('', this.createCat)
   }
 
 
   async getAllCats(request, response, next) {
-    const cats = await catsService.getAllCats()
-    response.send(cats)
+    try {
+      const cats = await catsService.getAllCats()
+      response.send(cats)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async createCat(request, response, next) {
+    try {
+      const cat = await catsService.createCat()
+      response.send(cat)
+    } catch (error) {
+      next(error)
+    }
   }
 }
--- src/services/CatsService.js
+++ src/services/CatsService.js
@@ -1,6 +1,9 @@
 import { dbContext } from "../db/DbContext.js"
 
 class CatsService {
+  createCat() {
+    throw new Error("Method not implemented.")
+  }
   async getAllCats() {
     // NOTE this is the mongoose find, not the javascript array method
     // NOTE find will return everything from a collection if no argument is passed to it
