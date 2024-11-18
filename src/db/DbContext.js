62207b3c19cafa22e6486d27913542ce3106e5bb
Jeremy Fowler <jembyfowler@gmail.com>
2024-11-18 12:12:10 -0700

    added collection to database

--- src/db/DbContext.js
+++ src/db/DbContext.js
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
 import { ValueSchema } from '../models/Value'
+import { CatSchema } from '../models/Cat.js'
 
 // this is similar to the appstate
 class DbContext {
-  Values = mongoose.model('Value', ValueSchema);
-  Account = mongoose.model('Account', AccountSchema);
+  Cats = mongoose.model('Cat', CatSchema)
 }
 
 export const dbContext = new DbContext()
